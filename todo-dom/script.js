03980c8c14cf75101377771c016d5d3c77db6081
AN-2000 <anirudh.inc.my.company@gmail.com>
2021-06-21 20:55:29 +0530

    update

--- todo-dom/script.js
+++ todo-dom/script.js
@@ -4,14 +4,21 @@ let btn = document.querySelector("button");
 
 //add event listener ek event ka wait kr rha h - click event,ki jb ye event hoga side me likha function chla dunga
 btn.addEventListener("click", function () {
-    //input box se value nikal rhe h
+  //input box se value nikal rhe h
   let task = input.value;
-//ek new li element create kr rhe hai
+  input.value = "";
+  if (task == "") return;
+
+  //ek new li element create kr rhe hai
   let li = document.createElement("li");
 
   //input box se jo value nikali thi wo is li element me dal rhe hai
   li.innerText = task;
 
-//   us li element ko jo abhi create kra just ul ke ander dal rhe hai
+  li.addEventListener("dblclick", function () {
+    li.remove();
+  });
+
+  //   us li element ko jo abhi create kra just ul ke ander dal rhe hai
   ul.append(li);
 });
