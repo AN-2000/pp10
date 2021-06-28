46dcc9c8af415758969dffaa26385e953bd40070
AN-2000 <anirudh.inc.my.company@gmail.com>
2021-06-28 21:29:00 +0530

    update

---
+++ jira/script.js
@@ -0,0 +1,10 @@
+let addBtn = document.querySelector(".add");
+let body = document.querySelector("body");
+
+addBtn.addEventListener("click", function () {
+  let div = document.createElement("div"); //<div></div>
+
+  div.classList.add("modal"); //<div class="modal"></div>
+
+  body.append(div);
+});
