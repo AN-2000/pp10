0963c935014e1d5c540907ece3556ae2b215fbef
AN-2000 <anirudh.inc.my.company@gmail.com>
2021-06-29 19:22:46 +0530

    update

--- jira/script.js
+++ jira/script.js
@@ -1,11 +1,37 @@
-// let addBtn = document.querySelector(".add");
-// let body = document.querySelector("body");
+let addBtn = document.querySelector(".add");
+let body = document.querySelector("body");
 
-// addBtn.addEventListener("click", function () {
+addBtn.addEventListener("click", function () {
+  let div = document.createElement("div"); //<div></div>
 
-//   let div = document.createElement("div"); //<div></div>
+  div.classList.add("modal"); //<div class="modal"></div>
 
-//   div.classList.add("modal"); //<div class="modal"></div>
+  div.innerHTML = ` <div class="task-section">
+  <div class="task-inner-container" contenteditable="true"></div>
+</div>
+<div class="modal-priority-section">
+  <div class="priority-inner-container">
+    <div class="modal-priority pink"></div>
+    <div class="modal-priority blue"></div>
+    <div class="modal-priority green"></div>
+    <div class="modal-priority black selected"></div>
+  </div>
+</div>`;
 
-//   body.append(div);
-// });
+  let allModalPriority = div.querySelectorAll(".modal-priority");
+
+  for (let i = 0; i < allModalPriority.length; i++) {
+    allModalPriority[i].addEventListener("click", function (e) {
+
+
+      for (let j = 0; j < allModalPriority.length; j++) {
+          allModalPriority[j].classList.remove("selected")
+      }
+
+      e.currentTarget.classList.add("selected")
+
+    });
+  }
+
+  body.append(div);
+});
