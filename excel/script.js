757ad458eb07cd42bc1f3415079d9a322eacad8a
AN-2000 <anirudh.inc.my.company@gmail.com>
2021-07-06 21:26:34 +0530

    up

--- excel/script.js
+++ excel/script.js
@@ -6,3 +6,19 @@ for(let i = 1;i<=100;i++){
     div.classList.add("row-number")
     rowNumberSection.append(div)
 }
+
+let columnTagsSection = document.querySelector(".column-tag-section")
+
+for(let i = 0;i<26;i++){
+
+    let asciiCode = 65 + i; 
+
+    let reqAlphabet = String.fromCharCode(asciiCode)
+
+    let div = document.createElement("div")
+    div.innerText = reqAlphabet;
+    div.classList.add("column-tag")
+    columnTagsSection.append(div)
+
+
+}
