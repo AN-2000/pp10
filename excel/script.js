0a720a46dc8a5f01df3a40c6fa231330394d48f4
AN-2000 <anirudh.inc.my.company@gmail.com>
2021-07-07 21:26:43 +0530

    up

--- excel/script.js
+++ excel/script.js
@@ -1,24 +1,53 @@
-let rowNumberSection = document.querySelector(".row-number-section")
+let rowNumberSection = document.querySelector(".row-number-section");
 
-for(let i = 1;i<=100;i++){
-    let div = document.createElement("div")
-    div.innerText = i
-    div.classList.add("row-number")
-    rowNumberSection.append(div)
+for (let i = 1; i <= 100; i++) {
+  let div = document.createElement("div");
+  div.innerText = i;
+  div.classList.add("row-number");
+  rowNumberSection.append(div);
 }
 
-let columnTagsSection = document.querySelector(".column-tag-section")
+let columnTagsSection = document.querySelector(".column-tag-section");
 
-for(let i = 0;i<26;i++){
+for (let i = 0; i < 26; i++) {
+  let asciiCode = 65 + i;
 
-    let asciiCode = 65 + i; 
+  let reqAlphabet = String.fromCharCode(asciiCode);
 
-    let reqAlphabet = String.fromCharCode(asciiCode)
+  let div = document.createElement("div");
+  div.innerText = reqAlphabet;
+  div.classList.add("column-tag");
+  columnTagsSection.append(div);
+}
 
-    let div = document.createElement("div")
-    div.innerText = reqAlphabet;
-    div.classList.add("column-tag")
-    columnTagsSection.append(div)
 
+let cellSection = document.querySelector(".cell-section")
+
+for (let i = 1; i <= 100; i++) {
+  let rowDiv = document.createElement("div");
+  rowDiv.classList.add("row");
+                       //i = 1 [A1,B1..........Z1]
+                       //i = 2 []
+                       //.
+                       //.
+                       //i = 100 [A100.........z100]
+
+  for (let j = 0; j < 26; j++) {       //i = 100   j = 25  asciiCode = 65+25=90  alpha = z  cellAdd = Z100
+    // A to Z
+    let asciiCode = 65 + j;
+
+    let reqAlphabet = String.fromCharCode(asciiCode);
+
+    let cellAddress = reqAlphabet + i;
+
+    let cellDiv = document.createElement("div");
+
+    cellDiv.classList.add("cell");
+
+    cellDiv.setAttribute("data-address", cellAddress);
+    rowDiv.append(cellDiv);
+  }
+
+  cellSection.append(rowDiv)
 
 }
