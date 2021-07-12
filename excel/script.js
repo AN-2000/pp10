b6725c6296c32cafc078c830ce980d315c09db62
AN-2000 <anirudh.inc.my.company@gmail.com>
2021-07-12 18:33:36 +0530

    up

--- excel/script.js
+++ excel/script.js
@@ -1,5 +1,9 @@
 let rowNumberSection = document.querySelector(".row-number-section");
 
+let formulaBarSelectedCellArea = document.querySelector(".selected-cell-div");
+
+let lastCell;
+
 for (let i = 1; i <= 100; i++) {
   let div = document.createElement("div");
   div.innerText = i;
@@ -20,19 +24,19 @@ for (let i = 0; i < 26; i++) {
   columnTagsSection.append(div);
 }
 
-
-let cellSection = document.querySelector(".cell-section")
+let cellSection = document.querySelector(".cell-section");
 
 for (let i = 1; i <= 100; i++) {
   let rowDiv = document.createElement("div");
   rowDiv.classList.add("row");
-                       //i = 1 [A1,B1..........Z1]
-                       //i = 2 []
-                       //.
-                       //.
-                       //i = 100 [A100.........z100]
+  //i = 1 [A1,B1..........Z1]
+  //i = 2 []
+  //.
+  //.
+  //i = 100 [A100.........z100]
 
-  for (let j = 0; j < 26; j++) {       //i = 100   j = 25  asciiCode = 65+25=90  alpha = z  cellAdd = Z100
+  for (let j = 0; j < 26; j++) {
+    //i = 100   j = 25  asciiCode = 65+25=90  alpha = z  cellAdd = Z100
     // A to Z
     let asciiCode = 65 + j;
 
@@ -42,12 +46,34 @@ for (let i = 1; i <= 100; i++) {
 
     let cellDiv = document.createElement("div");
 
+    // cellDiv.contentEditable = true
+
+    cellDiv.setAttribute("contentEditable", true);
+
     cellDiv.classList.add("cell");
 
     cellDiv.setAttribute("data-address", cellAddress);
+
+    cellDiv.addEventListener("click", function (e) {
+      if (lastCell) {
+        lastCell.classList.remove("cell-selected");
+      }
+
+      e.currentTarget.classList.add("cell-selected");
+
+      lastCell = e.currentTarget;
+
+
+
+      let currCellAddress = e.currentTarget.getAttribute("data-address")
+
+      formulaBarSelectedCellArea.innerText = currCellAddress
+
+
+    });
+
     rowDiv.append(cellDiv);
   }
 
-  cellSection.append(rowDiv)
-
+  cellSection.append(rowDiv);
 }
