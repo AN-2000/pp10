ef0cd6c07fe75a66ba0cfc73f690ab718382f3c1
AN-2000 <anirudh.inc.my.company@gmail.com>
2021-07-20 20:48:22 +0530

    added color picker

--- excel/styleScript.js
+++ excel/styleScript.js
@@ -1,8 +1,14 @@
 let allAlignmentOptions = document.querySelectorAll(".align-cell-content span");
+let allColorOptions = document.querySelectorAll(".cell-color-options span");
+
+let body = document.querySelector("body");
 
 let leftAlign = allAlignmentOptions[0];
 let centerAlign = allAlignmentOptions[1];
 let rightAlign = allAlignmentOptions[2];
+
+let bgColorPicker = allColorOptions[0];
+let fontColorPicker = allColorOptions[1];
 
 leftAlign.addEventListener("click", function () {
   if (lastCell) {
@@ -27,3 +33,36 @@ centerAlign.addEventListener("click", function () {
     dataObj[address].align = "center";
   }
 });
+
+bgColorPicker.addEventListener("click", function () {
+  let colorPickerElement = document.createElement("input");
+  colorPickerElement.type = "color";
+  body.append(colorPickerElement);
+  colorPickerElement.click();
+
+  colorPickerElement.addEventListener("input", function (e) {
+    console.log(e.currentTarget.value);
+    if (lastCell) {
+      lastCell.style.backgroundColor = e.currentTarget.value;
+      let address = lastCell.getAttribute("data-address");
+      dataObj[address].bgColor = e.currentTarget.value;
+    }
+  });
+});
+
+
+fontColorPicker.addEventListener("click", function () {
+    let colorPickerElement = document.createElement("input");
+    colorPickerElement.type = "color";
+    body.append(colorPickerElement);
+    colorPickerElement.click();
+  
+    colorPickerElement.addEventListener("input", function (e) {
+      console.log(e.currentTarget.value);
+      if (lastCell) {
+        lastCell.style.color = e.currentTarget.value;
+        let address = lastCell.getAttribute("data-address");
+        dataObj[address].color = e.currentTarget.value;
+      }
+    });
+  });
