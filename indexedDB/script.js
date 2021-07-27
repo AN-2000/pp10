37a088122fb605aaf7afe24ed4fdaaa74ce3776a
AN-2000 <anirudh.inc.my.company@gmail.com>
2021-07-27 18:39:45 +0530

    up

---
+++ indexedDB/script.js
@@ -0,0 +1,19 @@
+//database version signifies how many major changes my database has been through
+let req = indexedDB.open("Notes", 1);
+
+req.addEventListener("success", function () {
+  console.log(1);
+  let db = req.result;
+  //   console.log(db);
+});
+
+//kuch bhi creation ya fir update ka kaam ho to ye wala apka event chlta h
+req.addEventListener("upgradeneeded", function () {
+  console.log(2);
+  let db = req.result;
+  console.log(db);
+});
+
+req.addEventListener("error", function () {
+  console.log(3);
+});
