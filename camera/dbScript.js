861702818abee2e9e1a8b6f052c6772bf7fe3b4b
AN-2000 <anirudh.inc.my.company@gmail.com>
2021-07-28 20:08:20 +0530

    viewMedia function

--- camera/dbScript.js
+++ camera/dbScript.js
@@ -4,7 +4,6 @@ let database;
 
 req.addEventListener("success", function () {
   database = req.result;
-  console.log(database);
 });
 req.addEventListener("upgradeneeded", function () {
   let db = req.result;
@@ -24,4 +23,63 @@ function saveMedia(media) {
   let tx = database.transaction("media", "readwrite");
   let mediaobjectStore = tx.objectStore("media");
   mediaobjectStore.add(data);
+}
+
+function viewMedia() {
+  if (!database) return;
+
+  let galleryContainer = document.querySelector(".gallery-container");
+
+  let tx = database.transaction("media", "readonly");
+  let mediaobjectStore = tx.objectStore("media");
+
+  let req = mediaobjectStore.openCursor();
+
+  req.addEventListener("success", function () {
+    cursor = req.result;
+    if (cursor) {
+      let mediaCard = document.createElement("div");
+
+      mediaCard.classList.add("media-card");
+
+      mediaCard.innerHTML = `<div class="actual-media"></div>
+      <div class="media-buttons">
+          <button class="media-download">Download</button>
+          <button class="media-delete">Delete</button>
+      </div>`;
+
+      let data = cursor.value.mediaData;
+
+      let actualMediaDiv = mediaCard.querySelector(".actual-media");
+
+      let type = typeof data;
+
+      if (type == "string") {
+        //image
+
+        let image = document.createElement("img");
+        image.src = data;
+
+        actualMediaDiv.append(image);
+      } else if (type == "object") {
+        //video
+        let video = document.createElement("video");
+
+        let url = URL.createObjectURL(data);
+
+        video.src = url;
+
+        video.autoplay = true
+        video.loop = true 
+        video.controls = true
+        video.muted = true
+
+        actualMediaDiv.append(video);
+      }
+
+      galleryContainer.append(mediaCard);
+
+      cursor.continue();
+    }
+  });
 }
