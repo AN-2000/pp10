f82e3fcceff166104b81b22288be7061ed95676c
AN-2000 <anirudh.inc.my.company@gmail.com>
2021-08-02 20:47:57 +0530

    up

---
+++ es6/arrow.js
@@ -0,0 +1,29 @@
+function fn1(x, y) {
+  console.log("Some process");
+  return x + y;
+}
+
+let fa1 = (x, y) => {
+  console.log("Some process");
+  return x + y;
+};
+
+// -----------------------------------
+
+function fn2(x) {
+  console.log("Some process");
+  return 2 * x;
+}
+
+let fa2 = x => {
+  console.log("Some process");
+  return 2 * x;
+};
+
+// -----------------------------------
+
+function fn3(x) {
+  return 3 * x;
+}
+
+let fa3 = x => 3 * x;
---
+++ es6/destructuringArr.js
@@ -0,0 +1,6 @@
+let a = [1, 2, 3, 4, 5];
+
+let [kuchBhi, , , reqVar] = a;
+
+console.log(kuchBhi);
+console.log(reqVar);
