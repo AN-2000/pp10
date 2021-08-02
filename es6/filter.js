d6ea6f40e368f2cd8c1b26ceaa91d03117081d44
AN-2000 <anirudh.inc.my.company@gmail.com>
2021-08-02 19:19:50 +0530

    up

--- es6/filter.js
+++ es6/filter.js
@@ -6,5 +6,26 @@ function isEven(x) {
 
 let filteredArr = a.filter(isEven);
 
+console.log("Original filter function:");
 console.log(a);
 console.log(filteredArr);
+
+// --------------------------
+
+// myFilter
+
+function myFilter(arr, f) {
+  let ans = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (f(arr[i])) {
+      ans.push(arr[i]);
+    }
+  }
+
+  return ans;
+}
+
+console.log("Created filter function:");
+console.log(a);
+console.log(myFilter(a, isEven));
--- es6/map.js
+++ es6/map.js
@@ -6,5 +6,24 @@ function double(x) {
 
 let ansArr = a.map(double);
 
+console.log("Original Map function:");
 console.log(a);
 console.log(ansArr);
+
+// ------------------------
+
+// myMap
+
+function myMap(arr, f) {
+  let ans = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    ans.push(f(arr[i]));
+  }
+
+  return ans;
+}
+
+console.log("Created Map function:");
+console.log(a);
+console.log(myMap(a, double));
