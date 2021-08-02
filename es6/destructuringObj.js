1fb9cbe641e9a645f9e8c8f0f7369c5449bb2cc3
AN-2000 <anirudh.inc.my.company@gmail.com>
2021-08-02 20:17:53 +0530

    up

---
+++ es6/destructuringObj.js
@@ -0,0 +1,13 @@
+let add = {
+  city: "Delhi",
+  region: "Pitampura",
+  pin: 110001,
+};
+
+let { region, pin, city, myVar } = add;
+
+console.log(region);
+console.log(pin);
+console.log(city);
+
+console.log(myVar);
