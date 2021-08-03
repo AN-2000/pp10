05dab5037956f633db467bf5b0558662b82aebbb
AN-2000 <anirudh.inc.my.company@gmail.com>
2021-08-03 18:28:28 +0530

    up

--- react/demo/src/index.js
+++ react/demo/src/index.js
@@ -1,5 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom";
+// Libraries
+
+import React from "react"; // creation logic
+
+import ReactDOM from "react-dom"; // render logic
+
+//App is a component we have imported
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+                <App />, // ek component
+                 document.querySelector("#root") // aur ek jagah jha us component ko dikhana hai
+                 );
