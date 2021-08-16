1fd58117a92f274bc03b55a034ff51ef101919d8
AN-2000 <anirudh.inc.my.company@gmail.com>
2021-08-16 19:46:42 +0530

    useEffect cleanup case 2

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -5,13 +5,18 @@ let App = () => {
 
   console.log("render was called");
 
+  //This is case 2 for clean  up
+  // in this case useEffect will only execute once and return a clean up function
+  //but we dont have other useEffect which will execute
+  // and we know clean up works before execution of useEffect
+  // so in this case the clean up execute when the component is getting unmounted from the screen
   useEffect(() => {
-    console.log("case 2 useEffect was called");
+    console.log("case 1 useEffect was called");
 
     return () => {
       console.log("clean up function");
     };
-  });
+  },[]);
 
   return (
     <div>
