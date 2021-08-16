df3e6668615b93c9f6646be64c0a5a6d505ca3c3
AN-2000 <anirudh.inc.my.company@gmail.com>
2021-08-16 19:36:02 +0530

    useEffect cleanup case 1

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -2,37 +2,16 @@ import { useEffect, useState } from "react";
 
 let App = () => {
   let [count, setCount] = useState(0);
-  let [process, setProcess] = useState("running");
 
   console.log("render was called");
 
-  // useEffect ek hook hai to functional component ke ander hi use hota hai
-  //it takes 2 arguments=> function, arr [optional]
-  // based on you have passed the arr or not
-  // we have 3 cases
-
-  //case 1:
-  // you have passed a function and an empty arr
-  // then useEffect calls the passed function only once, after first render
-  // so it works like componentDidMount
-  // useEffect(() => {
-  //   console.log("use effect was called");
-  // }, []);
-
-  // case 2:
-  // in this case you only give a function and no arr
-  // useEffect will execute your function after every render, that is after first render and every re-render
-  // useEffect(()=>{
-  //   console.log("case 2 useEffect was called");
-  // })
-
-  // case 3:
-  // this useEffect will execute after first render 
-  // and also after the state variable which is being used changes 
   useEffect(() => {
-    let arr = process.split("i");
-    console.log(arr);
-  }, [process]);
+    console.log("case 2 useEffect was called");
+
+    return () => {
+      console.log("clean up function");
+    };
+  });
 
   return (
     <div>
@@ -50,15 +29,6 @@ let App = () => {
         }}
       >
         -
-      </button>
-
-      <p>{process}</p>
-      <button
-        onClick={() => {
-          setProcess("stop");
-        }}
-      >
-        kill process
       </button>
     </div>
   );
