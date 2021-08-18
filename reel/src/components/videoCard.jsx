2445bf6b68c433e1edecba6370424c83578bbff3
AN-2000 <anirudh.inc.my.company@gmail.com>
2021-08-18 21:03:16 +0530

    partial ui complete

--- reel/src/components/videoCard.jsx
+++ reel/src/components/videoCard.jsx
@@ -3,20 +3,59 @@ import "./videoCard.css";
 
 let VideoCard = () => {
   let [playing, setPlaying] = useState(false);
+  let [commentBoxOpen, setCommentBoxOpen] = useState(false);
 
   return (
     <div className="video-card">
       <p className="video-card-username">Fake user</p>
       <span className="video-card-music">
-        <span class="material-icons">music_note</span>
+        <span className="material-icons">music_note</span>
         <marquee>some song</marquee>
       </span>
 
-      <span class="material-icons-outlined video-card-comment">chat</span>
+      <span
+        onClick={(e) => {
+          if (commentBoxOpen) {
+            setCommentBoxOpen(false);
+          } else {
+            setCommentBoxOpen(true);
+          }
+        }}
+        className="material-icons-outlined video-card-comment"
+      >
+        chat
+      </span>
 
-      <span class="material-icons-outlined video-card-like">
+      <span className="material-icons-outlined video-card-like">
         favorite_border
       </span>
+
+      {commentBoxOpen ? (
+        <div className="video-card-comment-box">
+          <div className="actual-comments">
+            <div className="post-user-comment">
+              <img src="https://images.unsplash.com/photo-1529665253569-6d01c0eaf7b6?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cHJvZmlsZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80" />
+              <div>
+                <h5>user name</h5>
+                <p>This is actual comment</p>
+              </div>
+            </div>
+            <div className="post-user-comment">
+              <img src="https://images.unsplash.com/photo-1529665253569-6d01c0eaf7b6?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cHJvZmlsZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80" />
+              <div>
+                <h5>user name</h5>
+                <p>This is actual comment</p>
+              </div>
+            </div>
+          </div>
+          <div className="comment-form">
+            <input type="text" />
+            <button>Post</button>
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
 
       <video
         onClick={(e) => {
