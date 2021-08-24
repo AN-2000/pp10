dc2dce3ca2c55b3c575073cffafd451ff5272258
AN-2000 <anirudh.inc.my.company@gmail.com>
2021-08-24 19:05:15 +0530

    ui completed

--- shopping-cart/src/components/preview.jsx
+++ shopping-cart/src/components/preview.jsx
@@ -1,5 +1,28 @@
+import "./preview.css";
+
 let Preview = () => {
-  return <>preview</>;
+  return (
+    <>
+      <div className="preview-container">
+        <div className="preview-img-container">
+          <img src="https://i.shgcdn.com/d28da852-3c05-408e-bde3-4aeb881e1a08/-/format/auto/-/preview/3000x3000/-/quality/lighter/" />
+        </div>
+
+        <div className="preview-listing">
+          <h2>Title</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Non iste
+            voluptas iusto earum reprehenderit sequi ratione quae numquam
+            ducimus ab assumenda ex, culpa, ipsam doloribus laborum sit vitae
+            delectus rem nam! Optio similique sunt, ut perferendis doloribus
+            nihil. Molestiae dolorem, voluptate eum vel deserunt commodi ut.
+            Ipsa ipsam cupiditate incidunt.
+          </p>
+          <button>Add to Cart</button>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Preview;
--- shopping-cart/src/components/product.jsx
+++ shopping-cart/src/components/product.jsx
@@ -4,10 +4,7 @@ let Product = () => {
   return (
     <div className="product-card">
       <div className="product-img">
-        <img
-          src="https://i.shgcdn.com/d28da852-3c05-408e-bde3-4aeb881e1a08/-/format/auto/-/preview/3000x3000/-/quality/lighter/"
-          alt=""
-        />
+        <img src="https://i.shgcdn.com/d28da852-3c05-408e-bde3-4aeb881e1a08/-/format/auto/-/preview/3000x3000/-/quality/lighter/" />
       </div>
       <div className="product-btn">
         <button>Add to Cart</button>
