735b1afd6f20665ffcb82f0012b68823587c5dd6
AN-2000 <anirudh.inc.my.company@gmail.com>
2021-08-30 18:16:48 +0530

    preview

--- resume-builder/src/components/preview.jsx
+++ resume-builder/src/components/preview.jsx
@@ -1,9 +1,27 @@
+import { useSelector } from "react-redux";
 import "./css/preview.css";
 
 let Preview = () => {
-  return <>
-        <div className="preview-container"></div>
-  </>;
+  let { fname, lname, city, state, email, phone, cgpa, college, degree, year } =
+    useSelector((state) => state.details);
+
+  let templateCode = useSelector((state) => state.template);
+  return (
+    <>
+      <div className="preview-container">
+        <p className={`template-${templateCode}`}>{fname}</p>
+        <p className={`template-${templateCode}`}>{lname}</p>
+        <p className={`template-${templateCode}`}>{email}</p>
+        <p className={`template-${templateCode}`}>{phone}</p>
+        <p className={`template-${templateCode}`}>{city}</p>
+        <p className={`template-${templateCode}`}>{state}</p>
+        <p className={`template-${templateCode}`}>{college}</p>
+        <p className={`template-${templateCode}`}>{degree}</p>
+        <p className={`template-${templateCode}`}>{cgpa}</p>
+        <p className={`template-${templateCode}`}>{year}</p>
+      </div>
+    </>
+  );
 };
 
 export default Preview;
